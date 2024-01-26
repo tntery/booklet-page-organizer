92ed5b0c29a8c84df5473760065e090a73f83e36
tntery <terychrystal@gmail.com>
2024-01-26 16:52:54 +0200

    Add pages arrangement method and logic for pages below 4.

--- main.py
+++ main.py
@@ -4,6 +4,27 @@ with arranging pages of a booklet so they
 can be printed with backing and adjacent 
 pages in the right order. 
 """
+
+def arrange_pages(total_num_pages :int) -> str:
+    """
+    Returns nicely formatted page combination and backing information from the supplied total number of pages.
+    @param:total_num_pages - total number of pages for the booklet.
+    """
+    pages_list = []
+    for i in range(1, total_num_pages + 1):
+        pages_list.append(i)
+
+    print("Pages list: ", pages_list)
+
+    arrangement_string = ""
+
+    while len(pages_list) > 0:
+        arrangement_string += f"{pages_list[0]} <-> {pages_list[-1]}\n"
+        pages_list.remove(pages_list[0])
+        pages_list.remove(pages_list[-1])
+
+    return arrangement_string
+
 clean_num_pages = 0
 input_error = False
 
@@ -28,8 +49,24 @@ while True:
         print("Exception", e)
 
 # Calculate and return results.
-response_text = "Hurray!!"
-print(response_text, clean_num_pages, "Pages")
+output_msgs_list = []
+total_inclusive_pages = 0
+
+    # For page total below 4
+if clean_num_pages < 4:
+    total_inclusive_pages = 4
+    num_blank_pages = 4 - clean_num_pages
+
+    output_msgs_list.append(f"You will need to add {num_blank_pages} more page(s) to your document or {num_blank_pages} blank pages at the end of your document so the booklet has enough pages to back each other out.")
+    output_msgs_list.append("Having done that, you can lay the following pages on the same sheet (one to the left, one to the right, e.g 1 <-> 2), then back that sheet with the '3 <-> 4' pages combination. \n\nBelow are your page combinations:")
+    output_msgs_list.append(arrange_pages((total_inclusive_pages)))
+
+else:
+    pass
+
+for msg in output_msgs_list:
+    print(msg)
+
 
 # If the result will include blank pages, present user with further options:
     # Where they want the blank pages (at the begining, at the end)
