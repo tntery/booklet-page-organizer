3fb255b4f849a2cda2d89245426a1ef81d6baacf
tntery <terychrystal@gmail.com>
2024-01-26 13:52:21 +0200

    Refactor initial user input code

--- main.py
+++ main.py
@@ -4,29 +4,35 @@ with arranging pages of a booklet so they
 can be printed with backing and adjacent 
 pages in the right order. 
 """
+clean_num_pages = 0
+input_error = False
 
 # Ask user to enter number of pages
-num_pages = input("Please enter total number of pages on your document:")
+while True:
 
-response_text = ""
+    if input_error:
+        num_pages = input("Please enter a VALID number for total number of pages on your document (Greater than 0)")
+    else:
+        num_pages = input("Please enter total number of pages on your document. \n(Must be a number greater than 0):")
 
-# while True:
-
-try:
-    num_blank_pages = int(num_pages) % 4
-    response_text = "Hurray!!"
-    print(response_text, num_blank_pages)
-    # break
-except ZeroDivisionError:
-    response_text = "Number of pages must be greater than 0"
-except Exception as e:
-    print("Exception", e)
-    response_text = "Please enter a number."
-
-input()
+    # Try and convert input to number and break if input is valid
+    try:
+        clean_num_pages = int(num_pages)
+        if clean_num_pages > 0:
+            input_error = False
+            break
+        else:
+            print("Please enter a valid number of pages (Greater than 0)")
+    except Exception as e:
+        input_error = True
+        print("Exception", e)
 
 # Calculate and return results.
+response_text = "Hurray!!"
+print(response_text, clean_num_pages, "Pages")
 
 # If the result will include blank pages, present user with further options:
     # Where they want the blank pages (at the begining, at the end)
     # Whether they can get rid of a certain number of pages or not.
+
+input()
